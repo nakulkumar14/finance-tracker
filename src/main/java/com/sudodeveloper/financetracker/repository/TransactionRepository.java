e094f88772460016ba6a03f45330662e5689ea42
nakul <nakulkumar14.09@gmail.com>
2025-05-17 12:29:32 +0530

    Added Transaction and repository

---
+++ src/main/java/com/sudodeveloper/financetracker/repository/TransactionRepository.java
@@ -0,0 +1,10 @@
+package com.sudodeveloper.financetracker.repository;
+
+import com.sudodeveloper.financetracker.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+  List<Transaction> findByUserId(Long userId);
+}
--- src/main/java/com/sudodeveloper/financetracker/repository/UserRepository.java
+++ src/main/java/com/sudodeveloper/financetracker/repository/UserRepository.java
@@ -2,11 +2,9 @@ package com.sudodeveloper.financetracker.repository;
 
 import com.sudodeveloper.financetracker.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Optional;
 
-@RestControllerAdvice
 public interface UserRepository extends JpaRepository<User, Long> {
 
   Optional<User> findByEmail(String email);
